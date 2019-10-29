b025d5e69944f6f11e3b29fb97e5c0b0edcfca6e
Mauricio <09mgr.15@gmail.com>
2019-10-29 15:01:49 -0700

    Querys updated

--- Querys/Views.sql
+++ Querys/Views.sql
@@ -4,3 +4,10 @@ select c.cria_id, corral_no, convert(varchar,cria_fechaL,106) as cria_fechaL, c.
 from crias c inner join clasificaciones cl on c.cria_id = cl.cria_id
 
 select * from dbo.CriasClasificadas
+
+
+create view dbo.CriasEnfermas
+as
+select c.cria_id, corral_no, cria_salud, cria_fechaL, cl.clas_grasCobertura
+from crias c inner join clasificaciones cl on c.cria_id = cl.cria_id
+where cria_salud in ('R','E')
